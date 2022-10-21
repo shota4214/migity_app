b0100298b36ede9bd44f3b6bc257253ab216f862
shota4214 <seshiruff4214@yahoo.co.jp>
2022-10-21 21:50:05 +0900

    :card_file_box: noteテーブルのdraft削除

--- db/migrate/20221021120745_create_notes.rb
+++ db/migrate/20221021120745_create_notes.rb
@@ -3,7 +3,6 @@ class CreateNotes < ActiveRecord::Migration[6.0]
     create_table :notes do |t|
       t.string :title, null: false
       t.text :content
-      t.boolean :draft, null: false, default: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -62,7 +62,6 @@ ActiveRecord::Schema.define(version: 2022_10_21_121646) do
   create_table "notes", force: :cascade do |t|
     t.string "title", null: false
     t.text "content"
-    t.boolean "draft", default: false, null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
