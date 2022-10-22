6d8dde74970a7ea8db29cde206373db004749d74
shota4214 <seshiruff4214@yahoo.co.jp>
2022-10-22 22:54:20 +0900

    :sparkles: Q&A投稿機能の実装

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,86 @@
+class QuestionsController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create edit update destroy]
+  before_action :set_question, only:%i[show edit update destroy change_resolved]
+
+  def index
+    @questions = Question.where(draft: false)
+    @questions = @questions.where(resolved: true) if params[:resolved]
+    @questions = @questions.where(resolved: false) if params[:unresolved]
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = current_user.questions.build(question_params)
+    if params[:back]
+      render :new
+    elsif params[:draft]
+      if @question.update(draft: true)
+      redirect_to questions_path, notice: "Q&Aを下書き保存しました"
+      else
+        render :new
+      end
+    elsif @question.save
+      redirect_to questions_path, notice: "Q&Aを投稿しました"
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if params[:draft]
+      @question.update(draft: true)
+      if @question.update(question_params)
+        redirect_to questions_path, notice: "Q&Aを編集し下書き保存しました"
+      else
+        render :edit
+      end
+    else
+      @question.update(draft: false)
+      if @question.update(question_params)
+        redirect_to questions_path, notice: "Q&Aを編集しました"
+      else
+        render :edit
+      end
+    end
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to questions_path, notice: "Q&Aを削除しました"
+  end
+
+  def confirm
+    @question = current_user.questions.build(question_params)
+    render :new if @question.invalid?
+  end
+
+  def change_resolved
+    if @question.resolved
+      @question.update(resolved: false)
+      redirect_to question_path(@question), notice: "Q&Aを回答受付中に変更しました"
+    else
+      @question.update(resolved: true)
+      redirect_to question_path(@question), notice: "Q&Aを解決済みに変更しました"
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :content, :resolved, :draft)
+  end
+
+  def set_question
+    @question = Question.find(params[:id])
+  end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @questions = @user.questions.all.order("created_at DESC")
   end
 end
