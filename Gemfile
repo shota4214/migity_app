92e6570b9c23d668f2af419d17fad72877f2d19a
shota4214 <seshiruff4214@yahoo.co.jp>
2022-10-23 18:02:02 +0900

    :sparkles: Q&A投稿rich_textの実装

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ gem 'rexml'
 gem 'devise-i18n'
 gem 'carrierwave'
 gem 'mini_magick'
+gem 'actiontext'
+gem 'image_processing', '~> 1.2'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/migrate/20221021120318_create_questions.rb
+++ db/migrate/20221021120318_create_questions.rb
@@ -2,7 +2,7 @@ class CreateQuestions < ActiveRecord::Migration[6.0]
   def change
     create_table :questions do |t|
       t.string :title, null: false
-      t.text :content
+      # t.text :content
       t.boolean :resolved, null: false, default: false
       t.boolean :draft, null: false, default: false
       t.references :user, null: false, foreign_key: true
