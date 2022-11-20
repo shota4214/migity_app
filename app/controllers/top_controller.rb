476d55ffd1a180ca7544773b006f1fca778fe7e6
shota4214 <seshiruff4214@yahoo.co.jp>
2022-11-20 10:58:16 +0900

    :art: トップページをrootパスに変更

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,25 @@
+class TopController < ApplicationController
+  before_action :other_than_drafts, only: %i[index search]
+  before_action :question_tag_ranks, only: %i[index show by_disease search]
+  before_action :sidebar_profession_users, only: %i[index show by_disease search]
+
+  def index
+    @questions = @questions.where(resolved: true).order("created_at DESC").page(params[:page]).per(10) if params[:resolved]
+    @questions = @questions.where(resolved: false).order("created_at DESC").page(params[:page]).per(10) if params[:unresolved]
+  end
+
+  private
+
+  def other_than_drafts
+    @questions = Question.where(draft: false).order("created_at DESC").page(params[:page]).per(10)
+  end
+
+  def question_tag_ranks
+    @question_tag_ranks = Disease.find(DiseaseLabelling.group(:disease_id).order('count(disease_id) desc').pluck(:disease_id))
+  end
+
+  def sidebar_profession_users
+    @sidebar_profession_users = User.joins(:comments).group("users.id", "comments.best_answer").having("comments.best_answer=true").order("count_all DESC").count
+    @sidebar_profession_users  = @sidebar_profession_users.map{|user, value| [User.find(user.first), value]}
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  root "questions#index"
+  root "top#index"
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
