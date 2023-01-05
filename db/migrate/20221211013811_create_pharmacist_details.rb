0afaabda3cea5b5f941cbc403fb0d74738b117b1
shota_suzuki <seshiruff4214@yahoo.co.jp>
2023-01-05 23:20:24 +0900

    :bug: 専門薬剤師詳細ページデプロイ時エラー対応pharmacist_detail/other_license,introductionにdefault&notnull追加

--- db/migrate/20221211013811_create_pharmacist_details.rb
+++ db/migrate/20221211013811_create_pharmacist_details.rb
@@ -3,8 +3,8 @@ class CreatePharmacistDetails < ActiveRecord::Migration[6.0]
     create_table :pharmacist_details do |t|
       t.string :office_name, null: false
       t.integer :license, null: false, default: 0
-      t.string :other_license
-      t.text :introduction
+      t.string :other_license, default: "", null: false
+      t.text :introduction, default: "", null: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
