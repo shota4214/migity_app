eb6d2a48b855496c385ce9bfbdcd3c5d1a9f5f9a
shota_suzuki <seshiruff4214@yahoo.co.jp>
2023-07-26 08:38:50 +0900

    aws-lightsail deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server '54.95.120.138', user: 'app', roles: %w{app db web}
+server '54.95.120.138', user: 'ec2-user', roles: %w{app db web}
 set :ssh_options, keys: '/Users/suzukishouta/.ssh/id_rsa'
